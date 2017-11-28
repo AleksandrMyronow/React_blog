51b923aacc55cbf285bdf0d88aa00f7002171315
Aleksandr Myronov <a.myronow@gmail.com>
2017-11-28 12:11:52 +0200

    end of first part

--- app.js
+++ app.js
@@ -1,7 +1,15 @@
 var express = require('express');
 var path = require('path');
+var bodyParser = require("body-parser");
 
 var app = express();
 app.use(express.static(path.join(__dirname,'/html')));
+app.use(bodyParser.json());
+
+app.post('/signin', (req, res) => {
+  var user_name=req.body.email;
+  var password=req.body.password;
+  user_name=='admin' && password=='admin' ? res.send('success') : res.send('Failure');
+});
 
 app.listen(8000, () => console.log('Started listening on port', 8000));
