b05690d721836d24acb5dd138afd9321d008863c
Aleksandr Myronov <a.myronow@gmail.com>
2017-11-28 10:19:32 +0200

    first commit

---
+++ app.js
@@ -0,0 +1,7 @@
+var express = require('express');
+var path = require('path');
+
+var app = express();
+app.use(express.static(path.join(__dirname,'/html')));
+
+app.listen(8000, () => console.log('Started listening on port', 8000));
