2c5b63b25c8f5f485ce8f004b06eaa528d4cae9f
Aleksandr Myronov <a.myronow@gmail.com>
2017-11-28 12:49:55 +0200

    dont work signup

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 var express = require('express');
 var path = require('path');
 var bodyParser = require("body-parser");
+var user = require('./user');
 
 var app = express();
 app.use(express.static(path.join(__dirname,'/html')));
@@ -10,6 +11,11 @@ app.post('/signin', (req, res) => {
   var user_name=req.body.email;
   var password=req.body.password;
   user_name=='admin' && password=='admin' ? res.send('success') : res.send('Failure');
+});
+
+app.post('/signup', function (req, res) {
+  user.signup('','','')
+  console.log(res);
 });
 
 app.listen(8000, () => console.log('Started listening on port', 8000));
