ea12d6df9491bf6508a54cba8ffb86af3977c0f6
Aleksandr Myronov <a.myronow@gmail.com>
2017-11-28 11:23:47 +0200

    add intro

--- html/main.jsx
+++ html/main.jsx
@@ -3,6 +3,7 @@ class Signin extends React.Component {
     super(props);
     this.handleEmailChange = this.handleEmailChange.bind(this);
     this.handlePasswordChange = this.handlePasswordChange.bind(this);
+    this.signIn = this.signIn.bind(this);
     this.state = {
       email: '',
       password: ''
@@ -15,6 +16,9 @@ class Signin extends React.Component {
   handlePasswordChange(e) {
     this.setState({password:e.target.value})
   }
+  signIn() {
+    alert('Email address is ' + this.state.email + ' Password is ' + this.state.password);
+  }
   render() {
     return(
       <form className="form-signin">
@@ -23,8 +27,7 @@ class Signin extends React.Component {
         <input type="email" onChange={this.handleEmailChange} id="inputEmail" className="form-control" placeholder="Email address" required autofocus />
         <label for="inputPassword" className="sr-only">Password</label>
         <input type="password" onChange={this.handlePasswordChange} id="inputPassword" className="form-control" placeholder="Password" required />
-
-        <button className="btn btn-lg btn-primary btn-block" type="button">Sign in</button>
+        <button className="btn btn-lg btn-primary btn-block" onClick={this.signIn} type="button">Sign in</button>
       </form>
     )
   }
