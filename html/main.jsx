0ad32fd76338f1d20aaa11bce408e6f979dfb12c
Aleksandr Myronov <a.myronow@gmail.com>
2017-11-28 10:36:06 +0200

    add form without logic

--- html/main.jsx
+++ html/main.jsx
@@ -1,12 +1,29 @@
 class Signin extends React.Component {
+  constructor(props) {
+    super(props);
+    this.handleEmailChange = this.handleEmailChange.bind(this);
+    this.handlePasswordChange = this.handlePasswordChange.bind(this);
+    this.state = {
+      email: '',
+      password: ''
+    };
+  }
+
+  handleEmailChange(e) {
+    this.setState({email:e.target.value})
+  }
+  handlePasswordChange(e) {
+    this.setState({password:e.target.value})
+  }
   render() {
     return(
       <form className="form-signin">
         <h2 className="form-signin-heading"> Please sign in </h2>
-        <label for="inputEmail" className="sr-only"> Email address</label>
-        <input type="email" id="inputEmail" className="form-control" placeholder="Email address" required autofocus />
-        <label for="inputPassword" className="sr-only"> Password</label>
-        <input type="password" id="inputPassword" className="form-control" placeholder="Password" required />
+        <label for="inputEmail" className="sr-only">Email address</label>
+        <input type="email" onChange={this.handleEmailChange} id="inputEmail" className="form-control" placeholder="Email address" required autofocus />
+        <label for="inputPassword" className="sr-only">Password</label>
+        <input type="password" onChange={this.handlePasswordChange} id="inputPassword" className="form-control" placeholder="Password" required />
+
         <button className="btn btn-lg btn-primary btn-block" type="button">Sign in</button>
       </form>
     )
